8be6237f1498a90ddaa376495da2ef26cb030cb4
Ronaldo <ronaldo@whera.com.br>
2018-06-15 09:31:21 -0300

    add new contants

--- src/Autoload/constants.php
+++ src/Autoload/constants.php
@@ -1,3 +1,27 @@
 <?php
 
 declare(strict_types=1);
+
+if (!defined('DS')) {
+    define('DS', DIRECTORY_SEPARATOR);
+}
+
+if (!defined('ROOT_DIR')) {
+    define('ROOT_DIR', dirname(dirname(__DIR__)) . DS);
+}
+
+if (!defined('APP_DIR')) {
+    define('APP_DIR', ROOT_DIR . 'app' . DS);
+}
+
+if (!defined('PUBLIC_DIR')) {
+    define('PUBLIC_DIR', ROOT_DIR . 'public' . DS);
+}
+
+if (!defined('CORE_DIR')) {
+    define('CORE_DIR', ROOT_DIR . 'src' . DS);
+}
+
+if (!defined('TMP_DIR')) {
+    define('TMP_DIR', ROOT_DIR . 'tmp' . DS);
+}
