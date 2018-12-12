c7bf6e9c5708d0c8e18cc574e7a930a12c57aa2c
Ronaldo Matos Rodrigues <ronaldo@whera.com.br>
2018-12-12 15:54:27 -0200

    test

---
+++ Test.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: ronaldo
+ * Date: 12/12/18
+ * Time: 15:52
+ */
+
+class test{
+
+    function __construct() {
+
+
+        // Eu sou um teste do codacy
+    }
+
+    public function ($x = [], $version) {
+        // PSR-2
+    }
+}
